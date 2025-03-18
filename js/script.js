ed183fc3ce33978751f72180f647e615c262ad3b
clonazpxn <personalmaildan.c@gmail.com>
2025-03-18 01:06:20 -0500

    MD3 added

--- js/script.js
+++ js/script.js
@@ -11,10 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
     let scrollTop = window.scrollY || document.documentElement.scrollTop;
 
     if (scrollTop > lastScrollTop && scrollTop > 50) {
-      // Al bajar, oculta la navbar
+      
       navbar.style.transform = "translateX(-50%) translateY(-100%)";
     } else {
-      // Al subir, muestra la navbar
+      
       navbar.style.transform = "translateX(-50%) translateY(0)";
     }
 
